05e07c1c668a697ecf34ffcaf7c12a2ed11048a9
ainiranasalsabil <ainiranasalsabil04@gmail.com>
2025-10-31 09:28:01 +0700

    Tambah struktur Column dengan padding atas 50dp dan alignment center

--- app/src/main/java/com/example/questuserinput_178/TeksEdit.kt
+++ app/src/main/java/com/example/questuserinput_178/TeksEdit.kt
@@ -1,8 +1,15 @@
 package com.example.questuserinput_178
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedCard
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.unit.dp
 import java.lang.reflect.Modifier
 
 @Composable
@@ -18,4 +25,15 @@ fun FormDataDiri(modifier: Modifier
     var jenis by remember { mutableStateOf("") }
 
     val gender:List<String> = listOf("Laki-laki","Perempuan")
+
+    Column(modifier =Modifier.padding(top =50.dp),
+        verticalArrangement = Arrangement.Top,
+
+
+
+        Row {
+
+        }
+    }
+
 }

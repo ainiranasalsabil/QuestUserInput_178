d6c35806f2ef80b85cbf4c33ade24e4249642461
ainiranasalsabil <ainiranasalsabil04@gmail.com>
2025-10-31 09:10:57 +0700

    membuat variable untuk menginput nilai masuk dari keyword

--- app/src/main/java/com/example/questuserinput_178/TeksEdit.kt
+++ app/src/main/java/com/example/questuserinput_178/TeksEdit.kt
@@ -1,5 +1,15 @@
 package com.example.questuserinput_178
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import java.lang.reflect.Modifier
 
 @Composable
+
+fun FormDataDiri(modifier: Modifier
+){
+    var textNama by remember { mutableStateOf("") }
+    var textAlamat by remember { mutableStateOf("") }
+    var textJK by remember { mutableStateOf("") }
+}

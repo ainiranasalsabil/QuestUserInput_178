97ca359eddfa04b76d83bd2e3572eda871f0f318
ainiranasalsabil <ainiranasalsabil04@gmail.com>
2025-10-31 09:55:59 +0700

    membuat row untuk jenis kelamin

--- app/src/main/java/com/example/questuserinput_178/TeksEdit.kt
+++ app/src/main/java/com/example/questuserinput_178/TeksEdit.kt
@@ -44,9 +44,16 @@ fun FormDataDiri(modifier: Modifier){
 
         Row{
             gender.forEach { item ->
-
+                Row(
+                    modifier = Modifier.selectable(
+                        selected = textJK == item,
+                        onClick = { textJK = item }
+                    ),
+                    verticalAlignment = Alignment.CenterVertically
+                )
             }
         }
 
 
+    }
 }

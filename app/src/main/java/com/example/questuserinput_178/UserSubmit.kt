1d86f580f3c111d131fa996ecb2335012b9af78c
ainiranasalsabil <ainiranasalsabil04@gmail.com>
2025-11-01 11:03:50 +0700

    menambahkan teks

--- app/src/main/java/com/example/questuserinput_178/UserSubmit.kt
+++ app/src/main/java/com/example/questuserinput_178/UserSubmit.kt
@@ -7,6 +7,8 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -16,6 +18,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 
 @Composable
 fun FormPendaftaranDataDiri(modifier: Modifier = Modifier) {
@@ -38,6 +42,10 @@ fun FormPendaftaranDataDiri(modifier: Modifier = Modifier) {
                 .height(dimensionResource(R.dimen.tinggi_header))
                 .background(Color.Blue),
     contentAlignment = Alignment.Center
-    )
+    ){
+        Text(
+
+        )
+    }
 
 }

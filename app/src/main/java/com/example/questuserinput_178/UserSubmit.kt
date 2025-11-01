140ccf425f384061ff6544af2028d7ad6e2fb41e
ainiranasalsabil <ainiranasalsabil04@gmail.com>
2025-11-01 10:47:43 +0700

    Menambahkan fungsi FormPendaftaranDataDiri sebagai dasar tampilan form

--- app/src/main/java/com/example/questuserinput_178/UserSubmit.kt
+++ app/src/main/java/com/example/questuserinput_178/UserSubmit.kt
@@ -1,2 +1,7 @@
 package com.example.questuserinput_178
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun FormPendaftaranDataDiri(modifier: Modifier = Modifier) {}

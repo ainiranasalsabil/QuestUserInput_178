6a43dd43b412b771e85b032c61bf72e9a78c5b13
ainiranasalsabil <ainiranasalsabil04@gmail.com>
2025-11-01 10:55:19 +0700

    Menambahkan komponen Column sebagai wadah utama tampilan

--- app/src/main/java/com/example/questuserinput_178/UserSubmit.kt
+++ app/src/main/java/com/example/questuserinput_178/UserSubmit.kt
@@ -1,11 +1,17 @@
 package com.example.questuserinput_178
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 
 @Composable
 fun FormPendaftaranDataDiri(modifier: Modifier = Modifier) {
@@ -16,5 +22,8 @@ fun FormPendaftaranDataDiri(modifier: Modifier = Modifier) {
 
     val gender = listOf("Laki-laki", "Perempuan")
     val status = listOf("Janda", "Lajang", "Duda")
+    Column(
+
+    )
 
 }

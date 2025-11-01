8148367557acaaf7eeccf8dee1fec286c64e8264
ainiranasalsabil <ainiranasalsabil04@gmail.com>
2025-11-01 10:58:39 +0700

    menambahkan pengaturan layout di dimens1

--- app/src/main/java/com/example/questuserinput_178/UserSubmit.kt
+++ app/src/main/java/com/example/questuserinput_178/UserSubmit.kt
@@ -2,8 +2,11 @@ package com.example.questuserinput_178
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -12,6 +15,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.dimensionResource
 
 @Composable
 fun FormPendaftaranDataDiri(modifier: Modifier = Modifier) {
